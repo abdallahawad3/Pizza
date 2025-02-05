ff535b61df69df3ac30844f8255756c86fe7e9d0
abdallahawad3 <djdgdnnxcbj@gmail.com>
2025-02-05 19:18:37 +0200

    refactor API functions for past orders and add price formatting hook

---
+++ src/hook/use-price-format.js
@@ -0,0 +1,8 @@
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export const priceConverter = (price) => intl.format(price);
+
+export const useCurrency = (price) => priceConverter(price);
